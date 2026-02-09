91e4c49937c1ec5664438aa7a7338bb82f898c2f
prabhuthedev03 <prabhuthedev@gmail.com>
2026-02-09 10:59:52 +0800

    ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export default {
   async fetch(request, env, ctx) {
-    return new Response("Hello World!");
+    // This looks for a matching file in your assets (like index.html)
+    // If it finds one, it serves it. If not, it returns a 404.
+    return await env.ASSETS.fetch(request);
   },
 };
